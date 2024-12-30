27cd26af907514abd67a7a4de2af00600e3f2f5a
suyash101101 <naharsuyash@gmail.com>
2024-12-31 04:57:36 +0530

    Updated sponsor details

--- src/components/Sponsors.js
+++ src/components/Sponsors.js
@@ -65,17 +65,54 @@ const Sponsors = () => {
           transition={{ duration: 0.8 }}
           className="text-center mt-20"
         >
-          <h2 className="text-4xl font-bold text-gray-800 mb-8">Wanna be a Sponsor?</h2>
+          <h2 className="text-4xl font-bold text-gray-800 mb-8">Information for Prospective Sponsors/Partners</h2>
           <div className="max-w-4xl mx-auto">
+            <p className="text-gray-600 mb-8 text-lg">
+              Thank You for showing your interest in sponsoring NITK-CREST 2025. The following Sponsorship categories available:
+            </p>
+            
             <img 
               src={sponsorshipImage} 
               alt="Sponsorship Details" 
               className="w-full h-auto rounded-lg shadow-lg mb-8"
             />
-            <div className="mt-8">
-              <h3 className="text-2xl font-semibold text-gray-700 mb-4">Contact Us and Become a Part</h3>
+
+            <div className="text-left mt-8 bg-gray-50 p-8 rounded-lg">
+              <p className="text-gray-600 mb-6">
+                However, if you wish to sponsor a custom amount, you can also contribute using the payment options shown above.
+              </p>
+
+              <h3 className="text-xl font-semibold text-gray-800 mb-4">
+                After the payment, please send an email to{' '}
+                <a href="mailto:nitk-crest2025@nitk.edu.in" className="text-blue-600 hover:underline">
+                  nitk-crest2025@nitk.edu.in
+                </a>
+                , with the following details:
+              </h3>
+              
+              <p className="text-gray-600 mb-2 italic">Please use the subject "Sponsorship towards NITK-CREST 2025"</p>
+              
+              <ul className="list-disc list-inside space-y-2 text-gray-600 ml-4 mb-6">
+                <li>Name of the Organization and address</li>
+                <li>Name of the contact Person</li>
+                <li>Email ID of the contact person</li>
+                <li>Mobile Number</li>
+                <li>Amount Paid</li>
+                <li>Date of Payment</li>
+                <li>Screenshot of the payment details (it must include Transaction Number/UTR Number)</li>
+                <li>Name on which the receipt has to be generated</li>
+                <li>GST Number (if any)</li>
+              </ul>
+
+              <p className="text-gray-600 mb-6">
+                You will be acknowledged with the payment receipt upon verifying your transaction details.
+              </p>
+
               <p className="text-gray-600">
-                Join us in making CREST 2025 a remarkable event. Reach out to us for sponsorship opportunities.
+                For further details, please write to us:{' '}
+                <a href="mailto:nitk-crest2025@nitk.edu.in" className="text-blue-600 hover:underline">
+                  nitk-crest2025@nitk.edu.in
+                </a>
               </p>
             </div>
           </div>
